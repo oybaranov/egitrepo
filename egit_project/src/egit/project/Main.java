216d0d95043f3d2dfa05932e160364cc247200e5
OB <OB@LAPTOP-QKEIGNO7.local>
2016-12-27 16:35:51 +0000

    git ecl 1

--- egit_project/src/egit/project/Main.java
+++ egit_project/src/egit/project/Main.java
@@ -4,11 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello Olga!!!");
+		System.out.println("Hello Yegor!!!");
 
 
 		System.out.println("Hello Yegor!!!");
-//comment
 
 	}
 
